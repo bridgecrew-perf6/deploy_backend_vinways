c25c9daa2db61aca84b2c854954d457144d45f81
Muhammad Rydwan <muhammadrydwanmylike10@gmail.com>
2020-12-15 17:54:24 +0700

    Fix typo on migration

--- migrations/20201203135518-create-music.js
+++ migrations/20201203135518-create-music.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Music", {
+    await queryInterface.createTable("Musics", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -41,6 +41,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("Music");
+    await queryInterface.dropTable("Musics");
   },
 };
